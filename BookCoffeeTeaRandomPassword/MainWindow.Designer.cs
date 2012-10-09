4169aeb9f65857aab0823cae3286d3c14c41dca7
Lu Rongkai <lurongkai@gmail.com>
2012-10-09 10:46:12 -0500

    git init.

---
+++ BookCoffeeTeaRandomPassword/MainWindow.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace BookCoffeeTeaRandomPassword
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.tbxPassword = new System.Windows.Forms.TextBox();
+            this.lblNowDate = new System.Windows.Forms.Label();
+            this.btnModifyRouter = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbxPassword
+            // 
+            this.tbxPassword.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbxPassword.BackColor = System.Drawing.Color.Silver;
+            this.tbxPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbxPassword.Font = new System.Drawing.Font("Calibri", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxPassword.ForeColor = System.Drawing.Color.Maroon;
+            this.tbxPassword.Location = new System.Drawing.Point(29, 42);
+            this.tbxPassword.Name = "tbxPassword";
+            this.tbxPassword.Size = new System.Drawing.Size(391, 79);
+            this.tbxPassword.TabIndex = 1;
+            this.tbxPassword.Text = "PASSWORD";
+            this.tbxPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblNowDate
+            // 
+            this.lblNowDate.AutoSize = true;
+            this.lblNowDate.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNowDate.ForeColor = System.Drawing.Color.Olive;
+            this.lblNowDate.Location = new System.Drawing.Point(12, 9);
+            this.lblNowDate.Name = "lblNowDate";
+            this.lblNowDate.Size = new System.Drawing.Size(35, 15);
+            this.lblNowDate.TabIndex = 2;
+            this.lblNowDate.Text = "Date:";
+            // 
+            // btnModifyRouter
+            // 
+            this.btnModifyRouter.Location = new System.Drawing.Point(130, 5);
+            this.btnModifyRouter.Name = "btnModifyRouter";
+            this.btnModifyRouter.Size = new System.Drawing.Size(75, 23);
+            this.btnModifyRouter.TabIndex = 3;
+            this.btnModifyRouter.Text = "Go";
+            this.btnModifyRouter.UseVisualStyleBackColor = true;
+            this.btnModifyRouter.Click += new System.EventHandler(this.btnModifyRouter_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(449, 133);
+            this.Controls.Add(this.btnModifyRouter);
+            this.Controls.Add(this.lblNowDate);
+            this.Controls.Add(this.tbxPassword);
+            this.MaximizeBox = false;
+            this.Name = "MainWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Generate Random Password";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxPassword;
+        private System.Windows.Forms.Label lblNowDate;
+        private System.Windows.Forms.Button btnModifyRouter;
+    }
+}
+
---
+++ BookCoffeeTeaRandomPassword/MainWindow.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Web;
+using System.Windows.Forms;
+
+namespace BookCoffeeTeaRandomPassword
+{
+    public partial class MainWindow : Form
+    {
+        private string password = "";
+        private SynchronizationContext sc = null;
+        public MainWindow() {
+            InitializeComponent();
+            sc = SynchronizationContext.Current;
+
+            GeneratePassword();
+            DisplayRandomPasswordAndDateTime();
+        }
+
+        private void DisplayRandomPasswordAndDateTime() {
+            lblNowDate.Text += DateTime.Now.ToShortDateString();
+            tbxPassword.Text = password;
+        }
+
+        private void GeneratePassword() {
+            var day = DateTime.Now.Date;
+            var passwordSeed = day.ToString();
+
+            var hashPassword = GetSHA1(passwordSeed);
+
+            password = hashPassword.Substring(0, 8);
+        }
+
+        private string GetSHA1(string text) {
+            UnicodeEncoding UE = new UnicodeEncoding();
+            byte[] hashValue;
+            byte[] message = UE.GetBytes(text);
+
+            SHA1Managed hashString = new SHA1Managed();
+            string hex = "";
+
+            hashValue = hashString.ComputeHash(message);
+            foreach (byte x in hashValue) {
+                hex += String.Format("{0:x2}", x);
+            }
+            return hex;
+        }
+
+        private const string AuthToken = "YWRtaW46YWRtaW4=";
+        private const string ModifyPasswordHead = "";
+        private void ModifyRouterWirelessPassword() {
+            var changePasswordAddress = String.Format("http://192.168.1.1/userRpm/WlanSecurityRpm.htm?vapIdx=1&secType=3&pskSecOpt=3&pskCipher=3&pskSecret={0}&interval=86400&wpaSecOpt=3&wpaCipher=1&radiusIp=&radiusPort=1812&radiusSecret=&intervalWpa=86400&wepSecOpt=3&keytype=1&keynum=1&key1=&length1=0&key2=&length2=0&key3=&length3=0&key4=&length4=0&Save=%B1%A3+%B4%E6 ", password);
+            var changePasswordRequest = HttpWebRequest.Create(changePasswordAddress);
+            AppendAuthHeader(changePasswordRequest.Headers);
+            changePasswordRequest.GetResponse();
+
+            var rebootAddress = "http://192.168.1.1/userRpm/SysRebootRpm.htm?Reboot=%D6%D8%C6%F4%C2%B7%D3%C9%C6%F7 ";
+            var rebootRequest = HttpWebRequest.Create(rebootAddress);
+            AppendAuthHeader(rebootRequest.Headers);
+            rebootRequest.GetResponse();
+        }
+
+        private void AppendAuthHeader(WebHeaderCollection headerCollection) {
+            headerCollection.Add("Authorization", "Basic " + AuthToken);
+        }
+
+        private void btnModifyRouter_Click(object sender, EventArgs e) {
+            Action action = ModifyRouterWirelessPassword;
+            btnModifyRouter.Enabled = false;
+            action.BeginInvoke(ar => {
+                sc.Post(spc => {
+                    ShowSuccess();
+                    btnModifyRouter.Enabled = true;
+                }, null);
+            }, null);
+        }
+
+        private void ShowSuccess() {
+            MessageBox.Show("Modify Success!");
+        }
+    }
+}
